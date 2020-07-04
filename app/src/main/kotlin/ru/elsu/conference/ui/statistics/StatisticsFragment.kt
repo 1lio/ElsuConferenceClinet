554ec5b8616d27e840fb3d9ddff675da99ab3c77
SukhovViacheslav <svo_efremov@mail.ru>
2020-07-04 21:29:58 +0300

    fix

--- app/src/main/kotlin/ru/elsu/conference/ui/statistics/StatisticsFragment.kt
+++ app/src/main/kotlin/ru/elsu/conference/ui/statistics/StatisticsFragment.kt
@@ -1,28 +1,6 @@
 package ru.elsu.conference.ui.statistics
 
-import android.os.Bundle
-import android.view.View
 import androidx.fragment.app.Fragment
 import ru.elsu.conference.R
 
-class StatisticsFragment : Fragment(R.layout.fr_statistics) {
-
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-     /*   val sort = requireActivity().findViewById<View>(R.id.sortView)
-        sort.visibility = View.GONE*/
-
-
-        /*
-
-        // Видимость кнопки
-        val params = fab.layoutParams as CoordinatorLayout.LayoutParams
-            params.anchorId = View.NO_ID
-            fab.layoutParams = params
-            fab.visibility = View.GONE
-        */
-
-    }
-}
+class StatisticsFragment : Fragment(R.layout.fr_statistics)
