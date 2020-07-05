83c369917fd89078531118d16d5f6a14bc144d37
SukhovViacheslav <svo_efremov@mail.ru>
2020-07-06 00:37:42 +0300

    Add parcel

--- app/src/main/kotlin/ru/elsu/conference/domain/model/Event.kt
+++ app/src/main/kotlin/ru/elsu/conference/domain/model/Event.kt
@@ -1,18 +1,71 @@
 package ru.elsu.conference.domain.model
 
-data class Event(
+import android.os.Parcel
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
 
-    val id: String,
-    val title: String,
-    val icon: String,
-    val status: Int = 0,
-    val time: String = "00:00",
-    val date: String = "01-10-2020",
-    val platform: String = "skype",
-    val members: Array<Member> = arrayOf(),
-    val institute: String = "none",
-    val description: String,
-    val lifecycle: Array<Int>,
-    val duration: String = "00:00:00",
-    val stars: Int = 5
-)
+@Entity
+data class Event(
+    @PrimaryKey(autoGenerate = false)
+    @SerializedName("id") var id: String,
+    @SerializedName("title") var title: String,
+    @SerializedName("icon") var icon: String,
+    @SerializedName("status") var status: Int = 0,
+    @SerializedName("time") var time: String = "00:00",
+    @SerializedName("date") var date: String = "01-10-2020",
+    @SerializedName("platform") var platform: String = "skype",
+    @SerializedName("members") var members: Array<Member> = arrayOf(),
+    @SerializedName("institute") var institute: String = "none",
+    @SerializedName("description") var description: String,
+    @SerializedName("lifecycle") var lifecycle: Array<Int>,
+    @SerializedName("duration") var duration: String = "00:00:00",
+    @SerializedName("stars") var stars: Int = 5
+) : Parcelable {
+
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readInt(),
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readArray(ClassLoader.getSystemClassLoader()) as Array<Member>,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        (parcel.readArray(ClassLoader.getSystemClassLoader()) as Array<Int>?)!!,
+        parcel.readString()!!,
+        parcel.readInt()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
+        parcel.writeString(title)
+        parcel.writeString(icon)
+        parcel.writeInt(status)
+        parcel.writeString(time)
+        parcel.writeString(date)
+        parcel.writeString(platform)
+        parcel.writeString(institute)
+        parcel.writeString(description)
+        parcel.writeString(duration)
+        parcel.writeInt(stars)
+    }
+
+    override fun describeContents(): Int = 0
+
+    companion object CREATOR : Parcelable.Creator<Event> {
+
+        override fun createFromParcel(parcel: Parcel): Event {
+            return Event(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Event?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
--- app/src/main/kotlin/ru/elsu/conference/domain/model/Member.kt
+++ app/src/main/kotlin/ru/elsu/conference/domain/model/Member.kt
@@ -1,12 +1,55 @@
 package ru.elsu.conference.domain.model
 
+import android.os.Parcel
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+@Entity
 data class Member(
-    val id: String,
-    val name: String,
-    val icon: String,
-    val skype: String,
-    val phone: String,
-    val mail: String,
-    val timeZone: String = "+3",
-    val assistant: String
-)
+    @PrimaryKey(autoGenerate = false)
+    @SerializedName("id") var id: String,
+    @SerializedName("name") var name: String,
+    @SerializedName("icon") var icon: String,
+    @SerializedName("skype") var skype: String,
+    @SerializedName("phone") var phone: String,
+    @SerializedName("mail") var mail: String,
+    @SerializedName("timeZone") var timeZone: String = "+3",
+    @SerializedName("assistant") var assistant: String
+) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
+        parcel.writeString(name)
+        parcel.writeString(icon)
+        parcel.writeString(skype)
+        parcel.writeString(phone)
+        parcel.writeString(mail)
+        parcel.writeString(timeZone)
+        parcel.writeString(assistant)
+    }
+
+    override fun describeContents(): Int = 0
+
+    companion object CREATOR : Parcelable.Creator<Member> {
+        override fun createFromParcel(parcel: Parcel): Member {
+            return Member(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Member?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
