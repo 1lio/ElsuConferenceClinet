e22f3738b702ed0c8351dfd8dee38b262c4b2b61
ViacheslavSukhov <svo_efremov@mail.ru>
2020-08-17 13:36:09 +0300

    Add Server Link

--- app/src/main/kotlin/ru/elsu/conference/core/AppConfig.kt
+++ app/src/main/kotlin/ru/elsu/conference/core/AppConfig.kt
@@ -2,6 +2,8 @@ package ru.elsu.conference.core
 
 object AppConfig {
 
-    const val BASE_URL = "127.0.0.1" // Started app in local host
+    // See server at https://github.com/1lio/Conference-REST
 
+    const val BASE_URL = "127.0.0.1" // Started app in local host
+    const val USE_FAKE = true        // Used fake data
 }
